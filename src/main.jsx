a4d7a69bc868175cdd30ee0e0ee48f642f5072ab
unknown <luthandodidiza197@gmail.com>
2023-08-31 11:26:40 +0200

    added scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import store from './store/store.js'
+import ScrollToTop from './Utils/ScrollToTop'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
   <Router>
     <App />
+    <ScrollToTop />
   </Router>
   </Provider>,
 )
